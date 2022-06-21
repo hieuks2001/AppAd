290209bb4b3b5dd334b48b04c7f80241f3ce75a9
Duong Le Ha <duongleha212001@gmail.com>
2022-06-21 23:20:38 +0700

    Add mission, change float point

---
+++ database/migrations/2022_06_21_093640_create_missions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('missions', function (Blueprint $table) {
+            $table->uuid('id')->primary()->unique();
+            $table->uuid('page_id');
+            $table->uuid('user_id');
+            $table->float('reward', 5, 4);
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('missions');
+    }
+}
