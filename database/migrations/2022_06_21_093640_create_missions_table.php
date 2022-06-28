61c49c7195f85e50e9b4b38da0c77ff5c5fdd819
Mavis2103 <taquanvku@gmail.com>
2022-06-28 23:52:17 +0700

    completed api getInfoOfSite and generateCode for mission

--- database/migrations/2022_06_21_093640_create_missions_table.php
+++ database/migrations/2022_06_21_093640_create_missions_table.php
@@ -6,32 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateMissionsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('missions', function (Blueprint $table) {
-            $table->uuid('id')->primary()->unique();
-            $table->uuid('page_id');
-            $table->uuid('user_id');
-            $table->float('reward', 5, 4);
-            $table->string('ip')->default('');
-            $table->string('user_agent')->default('');
-            $table->tinyInteger('status');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('missions', function (Blueprint $table) {
+      $table->uuid('id')->primary()->unique();
+      $table->uuid('page_id');
+      $table->uuid('user_id');
+      $table->float('reward', 5, 4);
+      $table->string('ip')->default('');
+      $table->string('user_agent')->default('');
+      $table->tinyInteger('status');
+      $table->string('code')->default('');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('missions');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('missions');
+  }
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"
         integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/ethjs@0.3.4/dist/ethjs.min.js"></script>
