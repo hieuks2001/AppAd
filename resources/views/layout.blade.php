612a1ba873a362f0081a78a667fdb944e9b123b1
Mavis2103 <taquanvku@gmail.com>
2022-07-18 16:09:20 +0700

    Dockerize

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,8 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta name="robots" content="noindex">
+  <meta name="googlebot" content="noindex">
   <meta
     name="viewport"
     content="width=device-width, initial-scale=1.0"
