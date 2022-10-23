9d3a57273b7b374abb3e63c915d42df9b18c012d
Mavis2103 <taquanvku@gmail.com>
2022-10-23 16:53:09 +0700

    fix cron not working

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,37 +9,37 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        UsersResetMissionCount::class,
-        ClearCodes::class,
-    ];
+  /**
+   * The Artisan commands provided by your application.
+   *
+   * @var array
+   */
+  protected $commands = [
+    UsersResetMissionCount::class,
+    ClearCodes::class,
+  ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-        $schedule->command('code:clear')->timezone("Asia/Ho_Chi_Minh")->at('00:00')->daily()->withoutOverlapping();
-    }
+  /**
+   * Define the application's command schedule.
+   *
+   * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+   * @return void
+   */
+  protected function schedule(Schedule $schedule)
+  {
+    // $schedule->command('inspire')->hourly();
+    $schedule->command('code:clear')->daily()->timezone("Asia/Ho_Chi_Minh")->at('00:00');
+  }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+  /**
+   * Register the commands for the application.
+   *
+   * @return void
+   */
+  protected function commands()
+  {
+    $this->load(__DIR__ . '/Commands');
 
-        require base_path('routes/console.php');
-    }
+    require base_path('routes/console.php');
+  }
 }
