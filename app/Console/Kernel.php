04b2171bf3db26c931fee013b99b0193fad1adbd
Mavis2103 <taquanvku@gmail.com>
2022-10-23 16:53:35 +0700

    fix cron not working

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('mission:reset')->timezone("Asia/Ho_Chi_Minh")->daily();
+        $schedule->command('mission:reset')->daily()->timezone("Asia/Ho_Chi_Minh")->at('00:00');
     }
 
     /**
