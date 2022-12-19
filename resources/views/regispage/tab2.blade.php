aad1ab16f69e226fdeeb474bff90e8a1373e2c63
Mavis2103 <taquanvku@gmail.com>
2022-12-19 19:15:40 +0700

    add button copy script

--- resources/views/regispage/tab2.blade.php
+++ resources/views/regispage/tab2.blade.php
@@ -11,6 +11,7 @@
       <th class="bg-slate-200">Tổng</th>
       <th class="bg-slate-200">Số tiền</th>
       <th class="bg-slate-200">Trạng thái</th>
+      <th class="bg-slate-200">Mã nhúng</th>
     </tr>
   <tbody>
     @foreach($pages as $key => $value)
@@ -32,6 +33,15 @@
       <td class="bg-white">Đã huỷ</td>
       @break
       @endswitch
+      <td class="bg-white">
+        <button class="btn btn-square btn-sm" onclick="copyCode('{{$value->id}}')">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+          </svg>
+        </button>
+      </td>
     </tr>
     @endforeach
   </tbody>
@@ -42,4 +52,9 @@
   <a class="btn btn-outline btn-sm {{!$pages->hasMorePages() ? 'btn-disabled' : ''}}"
     href="{{$pages->nextPageUrl()}}">Next</a>
 </div>
+<script>
+  function copyCode(pageId) {
+    navigator.clipboard.writeText(document.getElementsByTagName("code")[0].textContent.replace('__code__',pageId))
+  }
+</script>
 @endsection
